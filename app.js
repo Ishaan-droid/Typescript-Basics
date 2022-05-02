144f6015591d9227a47087d3d4c5e1887ea8d255
Ishaan Thakur <ishaant99@gmail.com>
2022-05-02 13:28:12 -0300

    Object and Array types

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-function add(n1, n2, result) {
-    console.log("reach");
-    return result && n1 + n2;
-}
-var number1 = 5;
-var number2 = 2.8;
-var showResult = true;
-var x = add(number1, number2, showResult);
-console.log(x);
+var person = {
+    name: "Ishaan",
+    age: 26
+};
+var activities = [];
+activities.push("Ishaan");
+console.log(person);
+console.log(activities);
---
+++ app.ts
@@ -0,0 +1,10 @@
+const person: object = {
+  name: "Ishaan",
+  age: 26,
+};
+
+const activities: string[] = []; // can be of any type as well
+
+activities.push("Ishaan");
+
+console.log(person);
