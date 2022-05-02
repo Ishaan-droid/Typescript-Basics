dced74a721ad3dbf899dd5a7da7c650daaf7ce90
Ishaan Thakur <ishaant99@gmail.com>
2022-05-02 13:02:10 -0300

    First Commit

---
+++ 1-basics.ts
@@ -0,0 +1,9 @@
+function add(n1: number, n2: number, result: boolean) {
+  return result && n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const showResult = true;
+
+add(number1, number2, showResult);
---
+++ app.js
@@ -0,0 +1,9 @@
+function add(n1, n2, result) {
+    console.log("reach");
+    return result && n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var showResult = true;
+var x = add(number1, number2, showResult);
+console.log(x);
