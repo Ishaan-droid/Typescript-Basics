77bdd12d3f9cf4e76f26f89fc04e1c4c101d06d2
Ishaan Thakur <ishaant99@gmail.com>
2022-05-03 12:19:27 -0300

    Updates

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-var ROLE;
-(function (ROLE) {
-    ROLE[ROLE["ADMIN"] = 5] = "ADMIN";
-    ROLE[ROLE["READ_ONLY"] = 100] = "READ_ONLY";
-    ROLE[ROLE["WRITE_ONLY"] = 200] = "WRITE_ONLY";
-})(ROLE || (ROLE = {}));
-var person = {
-    name: 'Ishaan',
-    age: 26,
-    role: ROLE.WRITE_ONLY
-};
-var activities = []; // can be of any type as well
-activities.push('Ishaan');
-console.log(person);
-// Tuple
-var fixed = [2, 'Author'];
-if (person.role === ROLE.WRITE_ONLY) {
-    console.log(ROLE.WRITE_ONLY);
+var union = 'I can be either string or number';
+var literalType = 'number-1';
+console.log(literalType);
+// Function types
+function add(number1, number2) {
+    return number1 + number2;
 }
-var ADMIN = ROLE.ADMIN;
-console.log(ADMIN);
+// This is a function type
+var displayAdd;
+displayAdd = add;
+console.log(displayAdd(2, 3));
