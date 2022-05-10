a5e822bbc7e4725d84ede10a61d2a1c68c56fbb4
Ishaan Thakur <ishaant99@gmail.com>
2022-05-10 09:36:03 -0300

    Generics and Type Constraints

--- 4-GenericsTypeScript/src/app.ts
+++ 4-GenericsTypeScript/src/app.ts
@@ -1,7 +1,7 @@
-const names: Array<string> = [];
+const names: Array<string> = []; // string[]
 const x = 3;
 
-const promise = new Promise((resolve, reject) => {
+const promise: Promise<string> = new Promise((resolve, reject) => {
   if (x === 3) {
     resolve('Got the data');
   } else {
@@ -10,3 +10,16 @@ const promise = new Promise((resolve, reject) => {
 });
 
 promise.then(res => console.log(res)).catch(err => console.log(err));
+
+const arr: Array<string | number> = ['m', 'x', 23];
+
+// Generics with type constraints
+
+function merge<T extends object, U extends object>(obj1: T, obj2: U) {
+  const obj3 = { ...obj1, ...obj2 };
+  return obj3;
+}
+
+const merged = merge({ name: 'Ishaan' }, { age: 26 });
+
+console.log(merged.age);
