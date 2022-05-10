faede843bde84692dac39d104cd9a20de580fbdf
Ishaan Thakur <ishaant99@gmail.com>
2022-05-10 09:52:26 -0300

    Another Generic Function

--- 4-GenericsTypeScript/src/app.ts
+++ 4-GenericsTypeScript/src/app.ts
@@ -23,3 +23,19 @@ function merge<T extends object, U extends object>(obj1: T, obj2: U) {
 const merged = merge({ name: 'Ishaan' }, { age: 26 });
 
 console.log(merged.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) {
+  const descriptionText = 'Got no value';
+
+  if (element.length === 1) console.log(`Got only 1 element.`);
+  else if (element.length > 1) console.log(`Got ${element.length} elements`);
+
+  return [element, descriptionText];
+}
+
+const count = countAndDescribe(['No', 'Yes']);
+console.log(count);
